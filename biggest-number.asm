8032c9d0b4c57909f6518e969e9ecd1c7d6f79b4
Mateus Novaes <mateussn13@hotmail.com>
2021-06-04 22:34:51 -0300

    comments refactoring

--- biggest-number.asm
+++ biggest-number.asm
@@ -11,19 +11,19 @@ value3 equ &fe ; Value 3 is equal to 0xfe
 
 org &8000		; Static start location in memory
 	ld b, value1	; Loads value1 to 'b' register
-	ld c, value2	; Loads value2 'c' register
-	ld d, value3	; Loads value3 'd' register
+	ld c, value2	; Loads value2 to 'c' register
+	ld d, value3	; Loads value3 to 'd' register
 
 	ld a, b		; Loads 'b' register to 'a' register
-	cp c		; Compares 'a' register to 'c' register
+	cp c		; Compares 'c' register to 'a' register
 	jp nc, next1	; Jumps to next1 label if not-carry(>=)
 	ld a, c		; Loads 'c' register to 'a' register
-	next1:		; Label or identification of an adress
-	cp d		; Compares 'a' register to 'd' register
+	next1:		; Label or identification of an address
+	cp d		; Compares 'd' register to 'a' register
 	jp nc, next2	; Jumps to next2 label if not-carry(>=)
 	ld a, d		; Loads 'd' register to 'a' register
 	next2:		; Another label
-	ld (&7000), a	; Loads acumulator to a specified adress 
-	ret		; Returns to main routine
+	ld (&7000), a	; Loads acumulator to a specified address 
+	ret		; Returns to the main routine
 		
  
